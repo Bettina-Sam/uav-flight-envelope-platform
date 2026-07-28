fcbcfb8d3cf10038af5c0caf05c349deae169831
Bettina Anne Sam <bettyliz1808@gmail.com>
2026-07-28 13:53:03 +0530

    Refresh PWA offline cache revision

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-a24bf94b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/offline.html",
-    "revision": "0.2ac5pf9p54o"
+    "revision": "0.lc7eqkosbfk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
